5ec1a09fdc1313b729ad576e4c76930560935f86
Kola Tella <kola.tella24@gmail.com>
2024-08-06 19:45:20 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,22 @@
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''Generates a random number from 0 to 10.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform() * 10
+
+
+
+# Example usage
+async def main():
+    async for number in async_generator():
+        print(number)
+
+if __name__ == "__main__":
+    asyncio.run(main())
